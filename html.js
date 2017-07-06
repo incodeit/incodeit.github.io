0886a45548e91a8395fc82fa50c7bda46aaa57de
Michele Mazzucco <hello@michelemazzucco.it>
2017-07-06 12:14:39 +0200

    first commit

---
+++ html.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { prefixLink } from 'gatsby-helpers'
+
+const BUILD_TIME = new Date().getTime()
+
+export default class HTML extends React.Component {
+  static propTypes = {
+    body: PropTypes.string
+  }
+
+  render() {
+    const head = Helmet.rewind()
+
+    let css
+    if (process.env.NODE_ENV === 'production') {
+      css = (
+        <style
+          dangerouslySetInnerHTML={{
+            __html: require('!raw!./public/styles.css'),
+          }}
+        />
+      )
+    }
+
+    return (
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" />
+          {head.title.toComponent()}
+          {head.meta.toComponent()}
+          {css}
+        </head>
+        <body>
+          <div
+            id="react-mount"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          <script src={prefixLink(`/bundle.js?t=${BUILD_TIME}`)} />
+        </body>
+      </html>
+    )
+  }
+}
---
+++ pages/_template.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import '../css/global.scss'
+
+const Template = props => <div>{ props.children }</div>
+
+export default Template
