6f9ecc4966a48bd31eb40bcbe98f0a6317a1dcad
Michele Mazzucco <hello@michelemazzucco.it>
2017-07-06 14:47:54 +0200

    fix link for cta

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Header = () => (
   <header>
-    <Logo />
+    { /* <Logo /> */ }
   </header>
 )
 
--- components/Hero/index.js
+++ components/Hero/index.js
@@ -7,7 +7,7 @@ const Hero = () => (
   <section className={style.hero}>
     <header className={style.header}>
       <h1 className={style.title}>We build <strong>web</strong>, <strong>app</strong>, and <strong>experiences</strong></h1>
-      <ButtonLink url="http://www.google.com" label="Contact us" />
+      <ButtonLink url="mailto:info@incode.it" label="Contact us" />
     </header>
   </section>
 )
