2689bfaa13ed73781390763211980df9415f07f7
Michele Mazzucco <hello@michelemazzucco.it>
2017-07-07 10:47:59 +0200

    add cname in pages

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+import Helmet from 'react-helmet'
+import { config } from 'config'
+
+export default class Index extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <p><em>(versione del 13/03/2012)</em></p>
+          <h4>PERCHE’ QUESTO AVVISO</h4>
+          In questa pagina si descrivono le modalità di gestione del sito, in riferimento al trattamento dei dati personali degli utenti che lo consultano.<br />
+          <br />
+          Si tratta di un’informativa che è resa anche ai sensi dell'art. 13 del d.lgs. n. 196/2003 - Codice in materia di protezione dei dati personali , a coloro che interagiscono con i servizi web della INCODE Snc di BRAVO Luca e MOLINARI Antonio,&nbsp; accessibili per via telematica a partire dall’indirizzo:<br />
+          <a href="http://www.incode.it" title="http://www.incode.it">http://www.incode.it</a>, corrispondente alla pagina iniziale del sito aziendale.<br />
+          <br />
+          L’informativa è resa solo per il sito della INCODE Snc di BRAVO Luca e MOLINARI Antonio&nbsp; e non anche per altri siti web eventualmente consultati dall’utente tramite link, di cui la predetta società, non è in alcun modo responsabile.<br />
+          L’informativa si ispira anche alla Raccomandazione n. 2/2001&nbsp;&nbsp;&nbsp;&nbsp; che le Autorità europee per la protezione dei dati personali, riunite nel Gruppo istituito dall’art. 29 della direttiva n. 95/46/CE, hanno adottato il 17 maggio 2001 per individuare alcuni requisiti minimi per la raccolta di dati personali on-line, e, in particolare, le modalità, i tempi e la natura delle informazioni che i titolari del trattamento devono fornire agli utenti quando questi si collegano a pagine web, indipendentemente dagli scopi del collegamento.<br />
+          <br />
+          La Raccomandazione e una descrizione di sintesi delle sue finalità sono riportate in questo sito, a mezzo di link.<br />
+          <br />
+          <h4>
+            IL “TITOLARE” DEL TRATTAMENTO</h4>
+          <p>A seguito della consultazione di questo sito possono essere trattati dati relativi a persone identificate o identificabili.&nbsp;Il “titolare” del loro trattamento è:<br />
+            INCODE Snc di BRAVO Luca e MOLINARI Antonio, con Sede legale: Via Montello 3/C, 46041 Asola (MN) e&nbsp; Sede operativa: Via Gonfalonieri&nbsp; 9, 26100 Cremona (CR). Recapiti: Tel. +39 0372.027375 - Fax +39 0376.1994152 - Email: <a href="mailto:info@incode.it">info@incode.it</a></p>
+          <h4>
+            LUOGO DI TRATTAMENTO DEI DATI</h4>
+          I trattamenti connessi ai servizi web di questo sito hanno luogo presso la predetta sede operativa e sono curati solo da personale tecnico dell’azienda, incaricato del trattamento, oppure da eventuali incaricati di occasionali operazioni di manutenzione od aggiornamento delle pagine del sito.<br />
+          Nessun dato derivante dal servizio web viene comunicato o diffuso.<br />
+          <br />
+          I dati personali forniti dagli utenti che inoltrano richieste di contatti o invio di materiale informativo (e-mail, richiesta di offerte, preventivi, invio di curriculum vitae , depliant, brochure, Cd-rom ecc.) sono utilizzati al solo fine di eseguire il servizio o la prestazione richiesta e sono comunicati a terzi nel solo caso in cui ciò sia a tal fine necessario (p.es. servizio di spedizione).<br />
+          <br />
+          <h4>
+            TIPI DI DATI TRATTATI</h4>
+          <h5>
+            Dati di navigazione</h5>
+          I sistemi informatici e le procedure software preposte al funzionamento di questo sito web acquisiscono, nel corso del loro normale esercizio, alcuni dati personali la cui trasmissione è implicita nell’uso dei protocolli di comunicazione di Internet.<br />
+          <br />
+          Si tratta di informazioni che non sono raccolte per essere associate a interessati identificati, ma che per loro stessa natura potrebbero, attraverso elaborazioni ed associazioni con dati detenuti da terzi, permettere di identificare gli utenti.<br />
+          <br />
+          In questa categoria di dati rientrano gli indirizzi IP o i nomi a dominio dei computer utilizzati dagli utenti che si connettono al sito, gli indirizzi in notazione URI (Uniform Resource Identifier) delle risorse richieste, l’orario della richiesta, il metodo utilizzato nel sottoporre la richiesta al server, la dimensione del file ottenuto in risposta, il codice numerico indicante lo stato della risposta data dal server (buon fine, errore, ecc.) ed altri parametri relativi al sistema operativo e all’ambiente informatico dell’utente.<br />
+          <br />
+          Questi dati vengono utilizzati al solo fine di ricavare informazioni statistiche anonime sull’uso del sito e per controllarne il corretto funzionamento e vengono cancellati immediatamente dopo l’elaborazione. I dati potrebbero essere utilizzati per l’accertamento di responsabilità in caso di ipotetici reati informatici ai danni del sito: salva questa eventualità, allo stato i dati sui contatti web non persistono per più di sette giorni.<br />
+          Dati forniti volontariamente dall’utente<br />
+          <br />
+          L’invio facoltativo, esplicito e volontario di posta elettronica agli indirizzi indicati su questo sito comporta la successiva acquisizione dell’indirizzo del mittente, necessario per rispondere alle richieste, nonché degli eventuali altri dati personali inseriti nella missiva.<br />
+          <br />
+          Specifiche informative di sintesi verranno eventualmente e progressivamente riportate o visualizzate nelle pagine del sito predisposte per particolari servizi a richiesta.<br />
+          <br />
+          <h4>
+            Consenso</h4>
+          Va qui ricordato che il consenso è regolato dall’art. 23 del&nbsp; d.lgs. n. 196/2003 - Codice in materia di protezione dei dati personali e i Casi nei quali può essere effettuato il trattamento senza consenso è regolato dall’art. 24 stesso decreto<br />
+          <br />
+          <h4>
+            COOKIES</h4>
+          Nessun dato personale degli utenti viene in proposito acquisito dal sito.<br />
+          Non viene fatto uso di cookies per la trasmissione di informazioni di carattere personale, né vengono utilizzati c.d. cookies persistenti di alcun tipo, ovvero sistemi per il tracciamento degli utenti.<br />
+          L’uso di c.d. cookies di sessione (che non vengono memorizzati in modo persistente sul computer dell’utente e svaniscono con la chiusura del browser) è strettamente limitato alla trasmissione di identificativi di sessione (costituiti da numeri casuali generati dal server) necessari per consentire l’esplorazione sicura ed efficiente del sito.<br />
+          I c.d. cookies di sessione utilizzati in questo sito evitano il ricorso ad altre tecniche informatiche potenzialmente pregiudizievoli per la riservatezza della navigazione degli utenti e non consentono l’acquisizione di dati personali identificativi dell’utente.<br />
+          <br />
+          <h4>
+            FACOLTATIVITA’ DEL CONFERIMENTO DEI DATI</h4>
+          A parte quanto specificato per i dati di navigazione, l’utente è libero di fornire i dati personali riportati negli eventuali&nbsp; moduli di richiesta ad INCODE Snc di BRAVO Luca e MOLINARI Antonio o comunque indicati in contatti con l'azienda per sollecitare l’invio di materiale informativo o di altre comunicazioni.<br />
+          Il loro mancato conferimento può comportare l’impossibilità di ottenere quanto richiesto.<br />
+          <br />
+          <h4>
+            MODALITA’ DEL TRATTAMENTO</h4>
+          I dati personali sono trattati con strumenti automatizzati per il tempo strettamente necessario a conseguire gli scopi per cui sono stati raccolti.<br />
+          Specifiche misure di sicurezza sono osservate per prevenire la perdita dei dati, usi illeciti o non corretti ed accessi non autorizzati.<br />
+          <br />
+          <h4>
+            DIRITTI DEGLI INTERESSATI</h4>
+          I soggetti cui si riferiscono i dati personali hanno il diritto in qualunque momento di ottenere la conferma dell’esistenza o meno dei medesimi dati e di conoscerne il contenuto e l’origine, verificarne l’esattezza o chiederne l’integrazione o l’aggiornamento, oppure la rettificazione (art. 7 del d.lgs. n. 196/2003).<br />
+          <br />
+          Ai sensi del medesimo articolo si ha il diritto di chiedere la cancellazione, la trasformazione in forma anonima o il blocco dei dati trattati in violazione di legge, nonché di opporsi in ogni caso, per motivi legittimi, al loro trattamento.<br />
+          <br />
+          Le richieste vanno rivolte alla INCODE Snc di BRAVO Luca e MOLINARI Antonio<br />
+          Ogni sforzo verrà fatto per rendere il più possibile interoperabili le funzionalità di questo sito con i meccanismi di controllo automatico della privacy disponibili in alcuni prodotti utilizzati dagli utenti.<br />
+          <br />
+          Considerando che lo stato di perfezionamento dei meccanismi automatici di controllo non li rende attualmente esenti da errori e disfunzioni, si precisa che il presente documento, pubblicato all’indirizzo&nbsp;http://incode.it/it/content/privacy-policy&nbsp;costituisce la “Privacy Policy” di questo sito che sarà soggetta ad aggiornamenti (restano le varie versioni consultabili al medesimo indirizzo).<br />
+          <br />
+        </div>
+      </div>
+    )
+  }
+}
