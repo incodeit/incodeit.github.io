21cc18620dd2257e8d4ddb87a538483d46f7a03e
Michele Mazzucco <hello@michelemazzucco.it>
2017-08-02 10:19:14 +0200

    remove gatsby custom config

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -8,11 +8,11 @@ const now = new Date().getFullYear()
 
 const Footer = () => (
   <footer className={style.container}>
-    <span className={style.colLeft}>
+    <span className={style.left}>
       <strong>&copy;2007-{now} Incode Snc di Bravo L. e Molinari A.</strong><br/>
       Cod Fisc. e P.Iva 01357300191 Iscr. Reg. Imprese di MN n. 01357300191 
     </span>
-    <span className={style.colRight}>
+    <span className={style.right}>
       <Link to={prefixLink('/privacy-policy/')}>Privacy policy</Link>
     </span>
   </footer>
